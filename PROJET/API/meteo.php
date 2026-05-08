589d9f02ca19d79d1114b37e03b40b1f4ee179bc
LousLauT <ls.laurentt@gmail.com>
2026-05-08 12:22:28 +0200

    Delete PROJET directory

--- PROJET/API/meteo.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$url = "https://api.openweathermap.org/data/2.5/weather?q=Ollioules&appid=80f37c3756fbb7aae8b1c143c35e816d&units=metric&lang=fr";
-
-$data = json_decode(file_get_contents($url), true);
-
-?>
